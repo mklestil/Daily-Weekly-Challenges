e8d5b38af02d7fbc20b0ba9fcc9230d7d7ec1d06
Martin Klestil <Martin.klestil@gmx.at>
2025-03-06 04:21:37 +0100

    Java_Copy update Error
    update text in ErrorHandler
    - from german to english

--- Weekly/Java/Java_Copy/src/main/java/at/mklestil/java_copy/view/ErrorHandler.java
+++ Weekly/Java/Java_Copy/src/main/java/at/mklestil/java_copy/view/ErrorHandler.java
@@ -6,8 +6,8 @@ import javafx.scene.control.Label;
 public class ErrorHandler {
     public static void showError(String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Fehler");
-        alert.setHeaderText("Ein Fehler ist aufgetreten!");
+        alert.setTitle("Error");
+        alert.setHeaderText("An error has occurred!");
         alert.setContentText(message);
         alert.showAndWait();
     }
