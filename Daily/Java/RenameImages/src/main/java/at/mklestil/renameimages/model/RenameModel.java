e8e9393a70aa61ec60f92eb0a0506751b494ff3c
Martin Klestil <Martin.klestil@gmx.at>
2025-04-22 09:54:56 +0200

    RenameModel
    Update Logic
    - finish logic in RenameModel

--- Daily/Java/RenameImages/src/main/java/at/mklestil/renameimages/model/RenameModel.java
+++ Daily/Java/RenameImages/src/main/java/at/mklestil/renameimages/model/RenameModel.java
@@ -8,13 +8,29 @@ import java.util.List;
 public class RenameModel {
     private MainView view;
     private String newName;
+    private String extension;
+
+    /**
+     * The RenameModel class,
+     * @param theview
+     */
 
     public RenameModel(MainView theview){
         view = theview;
     }
 
+    /**
+     * Renames a list of files by applying a new base name and appending a running number.
+     * <p>
+     * Example: if newName is "Image", files will be renamed to "Image1.png", "Image2.png", etc.
+     * Existing extensions are preserved (currently fixed to .png).
+     *
+     * @param fileList the list of files to rename
+     */
     public void renameFiles(List<File> fileList) {
-        System.out.println("Rename Button Handler start");
+        /**
+         * Check input textfield
+         * */
         if(view.getInputField().getText() != null && !view.getInputField().getText().isEmpty()){
             newName = view.getInputField().getText();
             System.out.println("New Name: " + newName);
@@ -22,18 +38,29 @@ public class RenameModel {
             System.out.println("Error, textfield is null");
         }
 
-        if(fileList != null){
+        /**
+         * Create new name and rename files, update List
+         */
+        if(fileList != null && !fileList.isEmpty()){
             int count = 0;
+            view.getObservableList().clear(); //clear List
+
             for(File file: fileList){
+                // New File Name
                 count++;
-                //TODO: Bugfix, wrong filename, to long path
-                File newFile = new File(file.getPath()+ newName + count + ".png");
-                System.out.println("New File: " + newFile);
-                file.renameTo(newFile);
+                File newFile = new File(file.getParent(),  newName + count + ".png");
+
+                //Check and rename Files
+                boolean success = file.renameTo(newFile);
+                if(success){
+                    System.out.println("Rename to: " + newFile);
+                    //Update List
+                    view.getObservableList().add(newFile.getName());
+                }else {
+                    System.out.println("Failed to rename: " + file.getName());
+                }
 
                 //Update UI
-                view.getObservableList().removeAll();
-                view.getObservableList().add(newFile.getName());
                 view.getListView().refresh();
 
             }
